0b23712bbce2c49921229e91a06cf4423b7f620c
Joseph Tai <1660523@student.hcmus.edu.vn>
2021-05-06 16:04:57 -0700

    update api core

--- learn-reactjs/src/App.js
+++ learn-reactjs/src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TodoFeature from "./features/Todo";
+import {NavLink, Redirect, Route, Switch} from "react-router-dom";
+import AlbumFeature from "./features/Album";
+import NotFound from "./components/NotFound";
+import productApi from "./api/productApi";
 
 function App() {
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const params = {
+                _limit: 10
+            };
+            const products = await productApi.getAll(params);
+            console.log(products);
+        };
+        fetchProducts().then();
+    }, []);
     return (
         <div className="App">
-            <TodoFeature/>
+            <div className="nav-link">
+                <p><NavLink to="/todos" activeClassName="active-menus">Todos</NavLink></p>
+                <p><NavLink to="/albums" activeClassName="active-menus">Albums</NavLink></p>
+            </div>
+            <Switch>
+                <Redirect from="/smiles" to="/" exact/>
+                <Route path="/todos" component={TodoFeature}/>
+                <Route path="/albums" component={AlbumFeature}/>
+                <Route component={NotFound}/>
+            </Switch>
         </div>
     );
 }
--- learn-reactjs/src/features/Todo/index.jsx
+++ learn-reactjs/src/features/Todo/index.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TodoList from "./components/TodoList";
-
-TodoFeature.propTypes = {
-    
-};
+import {useLocation, useHistory, useRouteMatch} from "react-router-dom";
+import queryString from "query-string"
+// const queryString = require("query-string")
+TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
     const initListTodo = [
@@ -23,26 +23,50 @@ function TodoFeature(props) {
             status: "new"
         }
     ]
+    const location = useLocation()
+    const history = useHistory()
+    const match = useRouteMatch()
     const [listTodo, setListTodo] = useState(initListTodo)
-    const [filter, setFiter] = useState("all")
+    const [filter, setFiter] = useState(() => {
+        const params = queryString.parse(location.search)
+        return params.status ? params.status : "all"
+    })
     const listTodoFilter = listTodo.filter(todo => (filter === "all" || todo.status === filter))
     const handleTodoClick = (index) => {
         let newListTodo = [...listTodo]
         newListTodo[index] = {
             ...newListTodo[index],
-            status : newListTodo[index].status === "new" ? "success": "new",
+            status: newListTodo[index].status === "new" ? "success" : "new",
         }
         setListTodo(newListTodo)
     }
     const handleFilterAll = () => {
-        setFiter("all")
+        const queryParams = {status: "all"}
+        history.push({
+            path: match.path,
+            search: queryString.stringify(queryParams)
+        })
     }
     const handleFilterSuccess = () => {
         setFiter("success")
+        const queryParams = {status: "success"}
+        history.push({
+            path: match.path,
+            search: queryString.stringify(queryParams)
+        })
     }
-    const handleFilterNew =() => {
+    const handleFilterNew = () => {
         setFiter("new")
+        const queryParams = {status: "new"}
+        history.push({
+            path: match.path,
+            search: queryString.stringify(queryParams)
+        })
     }
+    useEffect(() => {
+        const params = queryString.parse(location.search)
+        setFiter(params.status ? params.status : "all")
+    }, [location.search])
     return (
         <div>
             <h3>My todo</h3>
